bfe515915f4045bf14c6e6f09efdb7cefad8ccf7
Henrik Wallerman <wallermanhenrik@gmail.com>
2022-04-03 13:34:52 +0200

    Category is now defined with jQuery

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-let categories = ["General Knowledge", "Entertainment: Books", "Entertainment: Film", "Entertainment: Music", "Entertainment: Musicals & Theatres", "Entertainment: Television", "Entertainment: Video Games", "Entertainment: Board Games", "Science & Nature", "Science: Computers", "Science: Mathematics", "Mythology", "Sports", "Geopraphy", "History", "Politics", "Art", "Celebrities", "Animals", "Vehicles", "Entertainment: Comics", "Science: Gadgets", "Entertainment: Japanese Anime & Manga", "Entertainment: Cartoon & Animation"];
+const categories = ["General Knowledge", "Entertainment: Books", "Entertainment: Film", "Entertainment: Music", 
+"Entertainment: Musicals & Theatres", "Entertainment: Television", "Entertainment: Video Games", 
+"Entertainment: Board Games", "Science & Nature", "Science: Computers", "Science: Mathematics", 
+"Mythology", "Sports", "Geopraphy", "History", "Politics", "Art", "Celebrities", "Animals", 
+"Vehicles", "Entertainment: Comics", "Science: Gadgets", "Entertainment: Japanese Anime & Manga", 
+"Entertainment: Cartoon & Animation"];
 
 categories.forEach(category => {
-    let option = document.createElement("option")
-    option.value = categories.indexOf(category) + 9;
-    option.innerHTML = category;
-    document.querySelector("#categorySelector").append(option);
+    let option = $("<option></option/>").text(category).val(categories.indexOf(category) + 9);
+    $("#categorySelector").append(option);
 });
 
 let getData = async(url) => {
@@ -28,7 +31,6 @@ let getData = async(url) => {
     let response = await fetch(url + queries);
     return await response.json();
 }
-
 
 let renderQuiz = async () => {
     let data = getData("https://opentdb.com/api.php");
